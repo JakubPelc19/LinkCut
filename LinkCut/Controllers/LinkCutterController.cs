cf20660941f28c284bd8aa7f74de124582c26137
Jakub Pelc <jakubpelc86@gmail.com>
2025-10-25 17:22:42 +0200

    LinkCutterController created

---
+++ LinkCut/Controllers/LinkCutterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkCut.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinkCutterController : ControllerBase
+    {
+    }
+}
