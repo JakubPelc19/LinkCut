59a1eb9a1158181cbc3a4c7fe4683b4436e17ff7
Jakub Pelc <jakubpelc86@gmail.com>
2025-10-25 16:52:44 +0200

    ShortLink model added

---
+++ LinkCut/Models/ShortLink.cs
@@ -0,0 +1,12 @@
+﻿namespace LinkCut.Models
+{
+    public class ShortLink
+    {
+        public int Id { get; set; }
+        
+        // Link provided by client
+        public Uri OriginalLink { get; set; }
+      
+        public string OriginalLinkId { get; set; }
+    }
+}
