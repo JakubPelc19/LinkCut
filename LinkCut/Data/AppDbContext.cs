07eef39e0785adf3ae5558f5f8c403eaa0c4ad9d
Jakub Pelc <jakubpelc86@gmail.com>
2025-10-25 16:55:58 +0200

    AppDbContext for EF Core added

---
+++ LinkCut/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LinkCut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkCut.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ShortLink> ShortLinks { get; set; }
+
+    }
+}
