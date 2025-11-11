7ed69c66629d289d72721b102966fc00d9bf39fa
Jakub Pelc <jakubpelc86@gmail.com>
2025-11-11 16:00:35 +0100

    CORS enabled

--- LinkCut/Program.cs
+++ LinkCut/Program.cs
@@ -3,11 +3,32 @@ using LinkCut.Services;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    try
+    {
 
+
+        string? allowedOrigins = builder.Configuration.GetValue<string>("AllowedOrigins");
+
+        string[]? allowedOriginsArray = allowedOrigins?.Split(";");
+        options.AddPolicy(name: MyAllowSpecificOrigins,
+            policy =>
+            {
+                policy.WithOrigins(allowedOriginsArray).WithHeaders("Content-Type").WithMethods("GET", "POST");
+            });
+    }
+    catch
+    {
+        throw new Exception("Couldn't find AllowedOrigins in appsettings.json");
+    }
+});
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -25,6 +46,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
